d6fe6550b9ae5c9cb739762ee6ef1012308d94a7
rke <imail.for.iwork@gmail.com>
2021-08-06 09:29:12 +0700

    nx add @ngneat/tailwind@latest

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './apps/**/*.{html,ts}',
+        './libs/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+      require('@tailwindcss/typography')
+    ],
+};
