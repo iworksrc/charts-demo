3b6ed93cc12f850cc1c271ea2988ef886f3f8120
rke <imail.for.iwork@gmail.com>
2021-08-06 12:18:19 +0700

    new widget

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartWidgetComponent } from './chart-widget/chart-widget.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    ChartWidgetComponent
+  ],
+  exports: [
+    ChartWidgetComponent
+  ]
 })
 export class UiModule {}
